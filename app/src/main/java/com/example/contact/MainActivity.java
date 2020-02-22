07abc20cbbfcbdbbaa19269eae3984f5a5fe6e9d
NMShihab <nmshihabislam@gmail.com>
2020-02-23 00:54:25 +0600

    Add Search

--- app/src/main/java/com/example/contact/MainActivity.java
+++ app/src/main/java/com/example/contact/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView = (ListView)findViewById(R.id.lview);
+        SearchView my_search_view = (SearchView)findViewById(R.id.search_bar);
 
         final ArrayList<String> arrayList = new ArrayList<>();
 
@@ -34,8 +36,9 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add("Apple");
         arrayList.add("Orange");
         arrayList.add("Banana");
+        arrayList.add("Cherry");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        final ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
 
         listView.setAdapter(arrayAdapter);
 
@@ -43,6 +46,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(MainActivity.this,"Clicked item : "+position+" "+arrayList.get(position).toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        my_search_view.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                arrayAdapter.getFilter().filter(s);
+                return false;
             }
         });
     }

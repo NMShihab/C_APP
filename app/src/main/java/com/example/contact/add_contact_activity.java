d72a584887bbf6de0120175a9f10f1907b48eb7c
NMShihab <nmshihabislam@gmail.com>
2020-02-26 10:58:29 +0600

    Contact activity

--- app/src/main/java/com/example/contact/add_contact_activity.java
+++ app/src/main/java/com/example/contact/add_contact_activity.java
@@ -3,6 +3,10 @@ package com.example.contact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class add_contact_activity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class add_contact_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact_activity);
+
+        final EditText name = (EditText)findViewById(R.id.name);
+        final EditText phn = (EditText)findViewById(R.id.phn);
+        final EditText mail = (EditText)findViewById(R.id.email);
+
+        Button add = (Button)findViewById(R.id.add);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Toast.makeText(add_contact_activity.this,"Name: "+name.getText().toString()+" Phn : "+
+                         phn.getText().toString()+ " Email: "+mail.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
